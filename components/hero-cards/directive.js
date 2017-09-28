d60393c74b508ebaee0c705be0435d3e87e3407f
Evan Ferris <Doombull@users.noreply.github.com>
2017-09-28 18:11:21 +1000

    Added hero selection and damage

--- components/hero-cards/directive.js
+++ components/hero-cards/directive.js
@@ -2,8 +2,23 @@ app.directive("heroCards",  function() {
     return {
         templateUrl : "components/hero-cards/template.html",
         scope: {
-            hero: "@",
+            hero: "=",
             recordDamage: "&"
+        },
+        link : function(scope, element, attrs) {
+            scope.hero.damage = [];
+
+            scope.recordDamage = function($event, increase) {
+                console.log('Got to test function');
+
+                if (increase && scope.hero.damage.length < scope.hero.maxHealth)
+                    scope.hero.damage.push({});
+                else if (!increase && scope.hero.damage.length > 0)
+                    scope.hero.damage.splice(0, 1);
+
+                    console.log(scope.hero.name + ': ' + scope.hero.damage.length);
+                $event.stopPropagation();
+            };
         }
     };
  });
--- scripts/app-controller.js
+++ scripts/app-controller.js
@@ -1,12 +1,67 @@
 app.controller('appController', function ($scope) {
     
-    $scope.addDamage = function($event, increase) {
-        console.log(Date.now() + ' Executing addDamage');
-        console.log(Date.now() + (increase ? ' Add damage' : ' Remove damage'));
-        $event.stopPropagation();
-    };
-    
     $scope.callHome = function (message) {
         alert(message);
     };
+
+    $scope.heros = {};
+    $scope.heros.all = [
+        {
+            name: 'Bastian Oriel',
+            maxHealth: 16
+        },
+        {
+            name: 'Brok',
+            maxHealth: 20,
+            selected: true
+        },
+        {
+            name: 'Doenrakkar',
+            maxHealth: 25
+        },
+        {
+            name: 'Istariel',
+            maxHealth: 15
+        },
+        {
+            name: 'Kruul',
+            maxHealth: 17
+        },
+        {
+            name: 'Kvarto',
+            maxHealth: 19
+        },
+        {
+            name: 'Piper',
+            maxHealth: 15
+        },
+        {
+            name: 'Rakkir',
+            maxHealth: 18
+        },
+        {
+            name: 'Sir Marcus',
+            maxHealth: 19
+        },
+        {
+            name: 'Skoll Bonestorm',
+            maxHealth: 27
+        },
+        {
+            name: 'Thorgar',
+            maxHealth: 23
+        },
+        {
+            name: 'Thrommel Ironbeard',
+            maxHealth: 22
+        },
+        {
+            name: 'Saiyin',
+            maxHealth: 15
+        },
+        {
+            name: 'Zaron Bogdan',
+            maxHealth: 16
+        }
+    ];
 });
